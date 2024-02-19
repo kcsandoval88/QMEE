b16b3d02b31ed56f48db939e2d3140c4b859ad71
kcsandoval88 <sandovk@mcmaster.ca>
2024-02-19 16:45:43 -0500

    combine cleaned data files into one

--- MyDataAssignment/practice1.R
+++ MyDataAssignment/practice1.R
@@ -74,5 +74,32 @@ for (sheet_name in sheet_names) {
   process_sheet(sheet_name)
 }
 
+library(tidyverse)
+library(openxlsx)
+
+
+
+# Specify the file paths for the cleaned data
+file_path_fam <- "MyDataAssignment/Cleaned_RCAMP_FAM.xlsx"
+file_path_unfam <- "MyDataAssignment/Cleaned_RCAMP_UNFAM.xlsx"
+
+# Read the cleaned data from both files
+cleaned_data_fam <- read.xlsx(file_path_fam)
+cleaned_data_unfam <- read.xlsx(file_path_unfam)
+
+# Create a new Excel workbook
+combined_file <- createWorkbook()
+
+# Add the cleaned data from RCAMP_FAM as the first sheet
+addWorksheet(combined_file, sheetName = "RCAMP_FAM")
+writeData(combined_file, sheet = "RCAMP_FAM", cleaned_data_fam)
+
+# Add the cleaned data from RCAMP_UNFAM as the second sheet
+addWorksheet(combined_file, sheetName = "RCAMP_UNFAM")
+writeData(combined_file, sheet = "RCAMP_UNFAM", cleaned_data_unfam)
+
+# Save the combined Excel file
+save_path <- "C:/PhD/QMEE/MyDataAssignment/Combined_RCAMP.xlsx"
+saveWorkbook(combined_file, file = save_path, overwrite = TRUE)
 
 #next step in a different R script
